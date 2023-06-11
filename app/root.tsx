d0f74c8e66e8ba77a666c2ee056eb61e4d78843f
Alex Pich <pich.alex01@gmail.com>
2023-06-11 14:25:45 -0700

    Add ErrorBoundary

--- app/root.tsx
+++ app/root.tsx
@@ -1,70 +1,77 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import type { LinksFunction } from "@remix-run/node";
 import {
-  Link,
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
+    Link,
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
 } from "@remix-run/react";
 
 import MainNavigation from "./components/MainNavigation";
 import styles from "./styles/main.css";
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <header>
-          <MainNavigation />
-        </header>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1"
+                />
+                <Meta />
+                <Links />
+            </head>
+            <body>
+                <header>
+                    <MainNavigation />
+                </header>
+                <Outlet />
+                <ScrollRestoration />
+                <Scripts />
+                <LiveReload />
+            </body>
+        </html>
+    );
 }
 
-export function ErrorBoundary({ error }: any) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-        <title>An error occurred!</title>
-      </head>
-      <body>
-        <header>
-          <MainNavigation />
-        </header>
-        <main>
-          <h1>An error occurred!</h1>
-          <p>{error.message}</p>
-          <p>
-            Back to <Link to="/">safety!</Link>
-          </p>
-        </main>
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+export function ErrorBoundary({ error }: { error: Error }) {
+    console.error(error);
+    return (
+        <html lang="en">
+            <head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1"
+                />
+                <Meta />
+                <Links />
+                <title>An error occurred!</title>
+            </head>
+            <body>
+                <header>
+                    <MainNavigation />
+                </header>
+                <main className="error">
+                    <h1>An error occurred!</h1>
+                    <p>{error?.message}</p>
+                    <p>
+                        Back to <Link to="/">safety</Link>!
+                    </p>
+                </main>
+                <ScrollRestoration />
+                <Scripts />
+                <LiveReload />
+            </body>
+        </html>
+    );
 }
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-  { rel: "stylesheet", href: styles },
+    ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+    { rel: "stylesheet", href: styles },
 ];
--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,41 +1,53 @@
 import { redirect } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
 import NoteList, { links as noteListLinks } from "~/components/NoteList";
 import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function NotesPage() {
-  const notes = useLoaderData();
+    const notes = useLoaderData();
 
-  return (
-    <main>
-      <NewNote />
-      <NoteList notes={notes} />
-    </main>
-  );
+    return (
+        <main>
+            <NewNote />
+            <NoteList notes={notes} />
+        </main>
+    );
 }
 
 export async function loader() {
-  const notes = await getStoredNotes();
-  return notes;
+    const notes = await getStoredNotes();
+    return notes;
 }
 
 export async function action({ request }: any) {
-  const formData = await request.formData();
-  const noteData = Object.fromEntries(formData);
-  // Add validation
+    const formData = await request.formData();
+    const noteData = Object.fromEntries(formData);
+    // Add validation
 
-  if (noteData.title.trim().length < 5) {
-    return { message: "Error: Title must 5 or more characters." };
-  }
+    if (noteData.title.trim().length < 5) {
+        return { message: "Error: Title must 5 or more characters." };
+    }
 
-  const existingNotes = await getStoredNotes();
-  noteData.id = new Date().toISOString();
-  const updatedNotes = existingNotes.concat(noteData);
-  storeNotes(updatedNotes);
-  return redirect("/notes");
+    const existingNotes = await getStoredNotes();
+    noteData.id = new Date().toISOString();
+    const updatedNotes = existingNotes.concat(noteData);
+    storeNotes(updatedNotes);
+    return redirect("/notes");
 }
 
 export function links() {
-  return [...newNoteLinks(), ...noteListLinks()];
+    return [...newNoteLinks(), ...noteListLinks()];
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+    return (
+        <main className="error">
+            <h1>An error related to your notes occurred!</h1>
+            <p>{error?.message}</p>
+            <p>
+                Back to <Link to="/">safety</Link>!
+            </p>
+        </main>
+    );
 }
